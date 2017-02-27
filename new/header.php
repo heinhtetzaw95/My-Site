1b5e481e85fe6f856c46ecea43016f22a1d2acb3
Hein Htet Zaw <thanlone1995@gmail.com>
2017-02-26 20:31:47 -0600

    added icons

--- new/header.php
+++ new/header.php
@@ -15,15 +15,30 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title> <?php echo "Zaw - ".ucwords($dirName); ?> </title>
-<link rel="stylesheet" type="text/css" href="./css/header.css">
+<title> <?php echo "HeinHtet.info - ".$dirName; ?> </title>
+
+<link rel="stylesheet" type="text/css" href="/css/header.css">
+<link rel="icon" type="image/png" href="/imgs/logo_white_bg.png" />
+
 </head>
 </body>
 <?php //this is test area
 
 ?>
-    <div id="header">
-          <img src="/imgs/logo.png" width="50px">
-    </div>
+  <div id="header">
+      <div id="site_identity">
+        <img id="header_logo" src="/imgs/logo_transparent.png">
+        <div id="header_name"> Hein Htet Zaw </div>
+      </div>
+      <div id="header_social_bar">
+          <ul id="social_bar">
+            <li><a href="https://www.facebook.com/heinhtetzaw95" target="_blank"> <img class="social_icons" src="/imgs/icons/round_fb.png"></a></li>
+            <li><a href="https://www.twitter.com/heinhtetzaw95" target="_blank"> <img class="social_icons" src="/imgs/icons/round_twitter.png"></a></li>
+            <li><a href="https://www.facebook.com/heinhtetzaw95" target="_blank"> <img class="social_icons" src="/imgs/icons/round_fb.png"></a></li>
+            <li><a href="https://www.facebook.com/heinhtetzaw95" target="_blank"> <img class="social_icons" src="/imgs/icons/round_fb.png"></a></li>
+
+          </ul>
+      </div>
+  </div>
 </body>
 </html>
