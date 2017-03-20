eccc1b5698fe45774188636fe078e03832ceeba5
Hein Htet Zaw <thanlone1995@gmail.com>
2017-03-20 14:39:43 -0500

    pdf viewer integrated

--- new/resume/resume.php
+++ new/resume/resume.php
@@ -1,21 +1,9 @@
-<?php
-  $resumeLink = "http://heinhtet.info/resume/Resume-HeinHtetZaw.pdf";
-?>
-
 <head>
   <link rel="stylesheet" type="text/css" href="/css/resume.css">
 </head>
-<div>
-
   <div>
-    <embed id="embeddedpdf" src="http://docs.google.com/gview?url=<?php echo $resumeLink?>&embedded=true"></embed>
   </div>
-<!--
-<embed src="/resume/resume.pdf">
--->
-</div>
+  <embed id="embeddedpdf" src="/pdfjs/web/viewer.html#zoom=100">
 <script>
-  $("#embeddedpdf").height(function(){
-    return $("#php_header").height() + $("#php_header_navbar").height() + $("#php_footer").height() + $(window).height() + 50;
-  });
+
 </script>
